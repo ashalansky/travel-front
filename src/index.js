6ba8aff26f8b879eb5a63c918d728b8bd555b2f0
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-09-28 12:51:15 -0600

    added compnent files

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nav from '../Nav';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-  console.log(process.env.REACT_APP_API_BASE_URL)
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
