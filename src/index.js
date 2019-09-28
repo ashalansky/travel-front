7cecaf7e8adbbff2d0d23c16f6dfc1466c8e00d6
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-09-27 18:00:13 -0600

    did nothing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+  console.log(process.env.REACT_APP_API_BASE_URL)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
