15bbab357beaf7f7efd97869dc127c303f2a6fd1
Scott <sroberts.kia@gmail.com>
2019-09-28 17:04:13 -0600

    added navbar component, added Destination cards

---
+++ src/stories/landing-CardGrid.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import CardGrid from "../mainpage/CardGrid";
+
+storiesOf("DestinationCards", module)
+.add("Grid", () => <CardGrid></CardGrid>);
