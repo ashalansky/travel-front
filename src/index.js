8e64805e7ab0343a88af3d50509cf401a94ca125
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-09-28 17:06:15 -0600

    added hero-banner

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Nav from '../Nav';
+// import Nav from 'modal/Nav';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
