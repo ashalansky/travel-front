d584b3e596155b4c3fe869532e5e4c27cb19e4bc
Amanda Shalansky <amandashalansky@Amandas-MBP.hitronhub.home>
2019-09-29 23:24:26 -0600

    npm installed animated.css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import NavBar from './mainpage/NavBar';
+import HeroBar from './mainpage/HeroBar';
+import CardGrid from './mainpage/CardGrid';
+import AppDescription from './mainpage/AppDescription';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
+export default function App() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Travel Bum
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <NavBar></NavBar>
+      <HeroBar></HeroBar>
+      <div><CardGrid></CardGrid></div>
+      <div><AppDescription></AppDescription></div>
     </div>
-  );
+  )
 }
+
+
+
