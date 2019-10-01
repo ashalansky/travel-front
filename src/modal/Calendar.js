51f139ff14d38b01722468d55be446527293e415
Andrew Kim <AndrewKim@Admin.local>
2019-09-30 18:09:17 -0600

    added auto-select

--- src/modal/Calendar.js
+++ src/modal/Calendar.js
@@ -4,14 +4,6 @@ import "./styles/calendar.css";
 import moment from 'moment';
 const classnames = require('classnames');
 
-const cities = [
-  "Calgary", "Edmonton", "Toronto"
-]
-
-const colors = [
-
-]
-
 const getDates = function (startDate, stopDate) {
   let dateArray = [];
   let currentDate = moment(startDate);
@@ -23,48 +15,57 @@ const getDates = function (startDate, stopDate) {
   return dateArray;
 };
 
+const setNewDate = function (departingDate) {
+  let departDate = moment(departingDate);
+  let newDate = moment(departDate).add(1, 'days');
+  return newDate
+}
+
 export default function CalendarComponent(props) {
   const [state, setState] = useState({
-    cities: cities,
     values: [],
-    city: "Calgary",
-    numberOfCities: 0
+    numberOfCities: 0,
+    firstDate: new Date(),
+    lastDate: new Date(),
   })
   
   const onChange = (values) => {
+    const arrivalDate = values[0];
+    const departingDate = values[1];
+    const dateArray = getDates(arrivalDate,departingDate);
+    const newDate = setNewDate(departingDate);
+    console.log(newDate);
     
-    const dateArray = getDates(values[0],values[1]);
-
     let newValues = state.values.concat([dateArray]);
     let cityNumber = state.numberOfCities + 1;
-    console.log(cityNumber);
-    setState({...state, values: newValues, numberOfCities: cityNumber})
+    if (state.values.length < 6) {
+      setState({...state, values: newValues, numberOfCities: cityNumber, firstDate: arrivalDate, lastDate: newDate["_d"]})
+    } else {
+      setState({...state, values: newValues, numberOfCities: cityNumber})
+    }
+
   }
-
-  const cityList = cities.map(city => {
-
-    return (
-      city
-    )
-
-  })
 
   return (
     <section>
-      {cityList}
       <Calendar
         calendarType="US"
         minDate={new Date()}
+        value = {state.lastDate}
         selectRange={true}
         onChange={(value) => {onChange(value)}}
         tileClassName={(tile) => {
-          let setActive = state.values.map((value) => {
-            if (tile.view === "month" && value.includes(tile.date.toDateString())) {
-              return classnames([`react-calendar__tile-selectedPrevious${state.numberOfCities}`])
-            } else {
-              return "react-calendar__tile"
+          let setActive = [];
+          for (let i = 0; (i< state.values.length) && (i < 6); i++) {
+            if (tile.view === "month" && state.values[i].includes(tile.date.toDateString()) && state.values[i].indexOf(tile.date.toDateString()) === 0) {
+              setActive.push(classnames([`react-calendar__tile-firstSelectedPrevious${i+1}`]))
+            } else if (tile.view === "month" && state.values[i].includes(tile.date.toDateString()) && state.values[i].indexOf(tile.date.toDateString()) === (state.values[i].length - 1)) {
+              setActive.push(classnames([`react-calendar__tile-lastSelectedPrevious${i+1}`])) 
+            } else if (tile.view === "month" && state.values[i].includes(tile.date.toDateString())) {
+              setActive.push(classnames([`react-calendar__tile-selectedPrevious${i+1}`]))
             }
-          })
+            
+          }
           return setActive
         }}
       />
