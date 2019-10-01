7c830ae60c2b717842c6f1f95615daa427afc85a
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-10-01 15:52:03 -0600

    created login component

--- src/mainpage/Login.js
+++ src/mainpage/Login.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Paper, Container } from '@material-ui/core'
+import { Paper, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    justify: 'center',
-    
+  grid: {
+    width: '40%',
+    justify: "center",
+    margin: 'auto'
   },
   paper: {
-    padding: 20,
+    padding: 30,
     marginTop: 10,
     marginBottom: 10,
-    textAlign: "center"
+    textAlign: "center",
+    alignItems: 'center',
+    boxShadow: '0px 11px 15px -7px rgba(0,0,0,0.2), 0px 24px 38px 3px rgba(0,0,0,0.14), 0px 9px 46px 8px rgba(0,0,0,0.12);',
+    fontFamily: 'Ubuntu'
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -24,17 +29,20 @@ export default function Login() {
   const classes = useStyles();
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <Container component="main" maxWidth="xs">
-        <Paper>
-        <TextField>
-          email
-        </TextField>
-        <TextField>
-          password
-        </TextField>
+      <Grid className={classes.grid} noValidate autoComplete="off">
+        <Paper className={classes.paper}>
+          <div>
+            <TextField variant='outlined' placeholder='Email'> 
+            </TextField>
+          </div>
+          <div>
+            <TextField variant='outlined' placeholder='Password'>
+            </TextField>
+          </div>
+          <Button variant="contained" color="primary" className={classes.button}>
+            Login
+          </Button>
         </Paper>
-      </Container>
-    </form>
+      </Grid>
   )
 }
