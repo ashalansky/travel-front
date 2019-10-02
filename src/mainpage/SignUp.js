8309f670673d43dbaa62c55048a136d6ba94a126
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-10-02 11:00:32 -0600

    changed navbar color

--- src/mainpage/SignUp.js
+++ src/mainpage/SignUp.js
@@ -18,20 +18,24 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     boxShadow: '0px 11px 15px -7px rgba(0,0,0,0.2), 0px 24px 38px 3px rgba(0,0,0,0.14), 0px 9px 46px 8px rgba(0,0,0,0.12);',
     fontFamily: 'Ubuntu',
-    width: '70%'
+    width: '70%',
+    borderRadius: 5
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     margin: theme.spacing(1),
-    width: '70%'
+    width: '70%',
+    fontFamily: 'Ubuntu'
   },
   title: {
     flexGrow: 1,
     margin: theme.spacing(1),
+    fontFamily: 'Ubuntu'
   },
   button: {
-    margin: theme.spacing(2)
+    margin: theme.spacing(2),
+    fontFamily: 'Ubuntu'
   }
 }));
 
